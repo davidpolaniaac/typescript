9a7b1a8512515b36476eee6b457b6ac01198faae
David Polania <15016075+davidpolaniaac@users.noreply.github.com>
2020-07-19 09:18:15 -0500

    type of data boolean

---
+++ type-of-data/main.ts
@@ -0,0 +1,17 @@
+let isSuperman: Boolean = true;
+let isBatman: Boolean = true;
+let isAcuaman: boolean = true;
+
+
+if (isSuperman) {
+    console.log("Yes !!! He is Superman");
+} else {
+    console.log("No ! he is not Superman");
+}
+
+isSuperman = convertToClark();
+
+
+function convertToClark(): Boolean {
+    return false;
+}
