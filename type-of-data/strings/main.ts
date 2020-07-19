70a452c4e9a6ee175b26b2f1d383b9a4ffa77ca3
David Polania <15016075+davidpolaniaac@users.noreply.github.com>
2020-07-19 09:35:35 -0500

    add type of data strings

---
+++ type-of-data/strings/main.ts
@@ -0,0 +1,21 @@
+let superman:string = "Superman";
+let batman:string = 'Batman';
+let flash:string = `Flash`;
+
+console.log(superman);
+console.log(batman);
+console.log(flash);
+
+let heroes:string = "The heroes are : " + superman +" "+ batman +" "+ flash;
+
+console.log(heroes);
+
+let concat:string = `The heroes are : ${superman} ${batman} ${flash}`;
+
+console.log(concat);
+
+
+
+
+
+
